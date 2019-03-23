5080e974ee6740f33b64ce70b0a6a7bf70bdc002
Matt Below <mbelow@gmail.com>
2019-03-23 11:48:19 -0400

    Updated the code to be nicer.

--- PedalKeyboardToggleKeys/PedalKeyboardToggleKeys.ino
+++ PedalKeyboardToggleKeys/PedalKeyboardToggleKeys.ino
@@ -1,105 +1,42 @@
 #include <Bounce2.h>
 #include <Keyboard.h>
-#include <Joystick.h>
 
 const int pedalPin = 3;
-const int buttonPin = 4;
-
-const int gamepadButton = 31;
+const int keyArraySize = 2;
+const char keyArray[keyArraySize] = "12";
 
 int previousPedalState = HIGH;
-int previousButtonState = HIGH;
-
-bool useGamePad = true;  //Default to gamepad
+int arrayPosition = 0;
 
 Bounce pedalBouncer = Bounce();
-Bounce buttonBouncer = Bounce();
-
-Joystick_ Joystick;
 
 void setup()
 {
   pinMode(pedalPin, INPUT_PULLUP);
-  pinMode(buttonPin, INPUT_PULLUP);
-
   pedalBouncer.attach(pedalPin); pedalBouncer.interval(50);
-  buttonBouncer.attach(buttonPin); pedalBouncer.interval(50);
-
-  Joystick.begin();
   Keyboard.begin();
 }
 
 void loop()
 {
   pedalBouncer.update();
-  buttonBouncer.update();
 
   int pedalVal = pedalBouncer.read();
-  int buttonVal = buttonBouncer.read();
 
-  //Toggle device to use.
-  if (buttonVal == LOW)
+  if (pedalVal == LOW && previousPedalState == HIGH)
   {
-    if (previousButtonState == HIGH)
-    {
-      if (useGamePad)
-      {
-        useGamePad = false;
-        //Release Gamepad Button
-        Joystick.releaseButton(gamepadButton);
-      }
-      else
-      {
-        useGamePad = true;
-        //Release Keyboard Button
-        Keyboard.releaseAll();
-      }
-      previousButtonState = LOW;
-    }
-    
-  }
-  if (buttonVal == HIGH)
-  {
-    if (previousButtonState == LOW)
-    {
-      previousButtonState = HIGH;
-    }
-  }
-  
-  if (pedalVal == LOW)
-  {
-    if (previousPedalState == HIGH)
-    {
-      if (useGamePad)
-      {
-        //Press Gamepad Button
-        Joystick.pressButton(gamepadButton);
-      }
-      else
-      {
-        //Press Keyboard Key(s)
-        Keyboard.press(KEY_F9);
-      }
-      previousPedalState = LOW;
-    }
-  }
-  if (pedalVal == HIGH)
-  {
-    if (previousPedalState == LOW)
-    {
-      if (useGamePad)
-      {
-        //Release Gamepad Button
-        Joystick.releaseButton(gamepadButton);
-      }
-      else
-      {
-        //Release Keyboard Button
-        Keyboard.releaseAll();
-      }
+    //Press Keyboard Key(s) and toggle between
+    Keyboard.print(keyArray[arrayPosition++]);
 
-      previousPedalState = HIGH;
-    }
+    //start at begining of array if end is met.
+    if (arrayPosition == keyArraySize)
+      arrayPosition = 0;
+      
+    previousPedalState = LOW;
+  }
+  if (pedalVal == HIGH && previousPedalState == LOW)
+  {
+    previousPedalState = HIGH;
   }
   delay(25);
 }
